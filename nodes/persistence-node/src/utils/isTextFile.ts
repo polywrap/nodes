9675cbaaa0d896ddf74f3bc53e82722b1c758f8d
mbrizic <brizic.mario@gmail.com>
2022-03-30 23:06:10 +0200

    Add more text formats

--- nodes/persistence-node/src/utils/isTextFile.ts
+++ nodes/persistence-node/src/utils/isTextFile.ts
@@ -1,4 +1,4 @@
-const textExtensions = [".graphql", ".yaml"]
+const textExtensions = [".graphql", ".yaml", ".txt", ".json", ".md"]
 
 export const isTextFile = (fileName: string): boolean => {
   return textExtensions.some(ext => fileName.endsWith(ext));
