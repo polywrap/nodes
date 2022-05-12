764c01d989a759efb4ab5c70516c1562c9a08279
Jure Bogunovic <bogunovij@gmail.com>
2022-05-13 01:38:59 +0200

    commented out wiping data on prod deploy

--- deployment/prod/update.sh
+++ deployment/prod/update.sh
@@ -27,10 +27,10 @@ pm2 delete ens-indexer-node-mainnet
 
 set -e
 
-rm $data/persistence-node -rf
-rm $data/ens-indexer-node/ropsten -rf
-rm $data/ens-indexer-node/rinkeby -rf
-rm $data/ens-indexer-node/mainnet -rf
+# rm $data/persistence-node -rf
+# rm $data/ens-indexer-node/ropsten -rf
+# rm $data/ens-indexer-node/rinkeby -rf
+# rm $data/ens-indexer-node/mainnet -rf
 
 mkdir -p $hosting/$persistenceNode
 cd $hosting/$persistenceNode
@@ -42,10 +42,10 @@ cd $hosting/$ensIndexerNode
 cp -r $staging/$ensIndexerNode/bin ./
 cp -r $staging/$ensIndexerNode/node_modules ./
 
-node $hosting/$persistenceNode/bin/main.js init --data $data/persistence-node --log
-node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/ropsten --network ropsten --log
-node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/rinkeby --network rinkeby --log
-node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/mainnet --network mainnet --log
+# node $hosting/$persistenceNode/bin/main.js init --data $data/persistence-node --log
+# node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/ropsten --network ropsten --log
+# node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/rinkeby --network rinkeby --log
+# node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/mainnet --network mainnet --log
 
 pm2 start $hosting/$persistenceNode/bin/main.js --name persistence-node -- daemon --data $data/persistence-node --gateway-port $persistenceNodePort --log
 pm2 start $hosting/$ensIndexerNode/bin/main.js --name ens-indexer-node-ropsten -- daemon --data $data/ens-indexer-node/ropsten --port $ensIndexerNodeRopstenPort -b $startingBlockRopsten --log
