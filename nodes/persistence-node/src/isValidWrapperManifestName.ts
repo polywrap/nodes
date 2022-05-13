97be2d99abef72b323f5a0ec97b34ec87e0f3357
Jure Bogunovic <bogunovij@gmail.com>
2022-05-13 16:18:29 +0200

    added a pins page with more info about wrappers

--- nodes/persistence-node/src/isValidWrapperManifestName.ts
+++ nodes/persistence-node/src/isValidWrapperManifestName.ts
@@ -1,3 +1,3 @@
 export const isValidWrapperManifestName = (fileName: string): boolean => {
-  return fileName === 'web3api.yaml' || fileName === 'web3api.json';
+  return fileName === 'web3api.yaml' || fileName === 'web3api.yml' || fileName === 'web3api.json';
 };
